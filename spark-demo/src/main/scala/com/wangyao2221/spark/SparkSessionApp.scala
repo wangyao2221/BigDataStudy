87a94284aed092364caba7703bdf9a05cd1a39ab
wangyao2221 <wangyao2221@163.com>
2019-08-03 23:53:37 +0800

    SparkSession

--- spark-demo/src/main/scala/com/wangyao2221/spark/SparkSessionApp.scala
+++ spark-demo/src/main/scala/com/wangyao2221/spark/SparkSessionApp.scala
@@ -8,20 +8,5 @@ object SparkSessionApp {
   def main(args: Array[String]): Unit = {
     val path = args(0)
 
-    // 1) 创建相关的Context
-    val sparkConf = new SparkConf()
-    sparkConf.setAppName("SQLContextApp").setMaster("local[2]")
-
-    val sc = new SparkContext(sparkConf)
-    val sqlContext = new SQLContext(sc)
-
-    // 2)相关的处理
-    val people = sqlContext.read.json(path)
-    people.printSchema()
-    people.foreach(row => println(row))
-
-    // 3)关闭资源
-    sc.stop()
-
   }
 }
